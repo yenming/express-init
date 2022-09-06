dd4c837fede6932b4bed11af50ac998a5c09930f
yenming <yenming0212@gmail.com>
2022-09-06 22:48:31 +0800

    init

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(function(err, req, res, next) {
 });
 
 // Listen on port 3000
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
     app.listen(port, () =>
       console.log("Express app listening on port " + port)
 );
